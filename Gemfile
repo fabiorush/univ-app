145e61072a76b6cefb64ef42bc07e3048ab9b172
Fábio Urquiza <fabiorush@gmail.com>
2020-02-09 17:19:38 -0300

    Add devise and google OAUTH2

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 gem "devise_materialize", "~>1.2.0"
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'about', to: 'pages#about'
   root 'courses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
